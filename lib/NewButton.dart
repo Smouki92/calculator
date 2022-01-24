29677fee9ba44c898f6311952c99be3b7c605339
Alehander <serheev24@gmail.com>
2022-01-24 19:10:02 +0200

    interface created

---
+++ lib/NewButton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NewButton extends StatelessWidget {
+  String text;
+
+
+
+  buttonPressed(String text){
+    print(text);
+  }
+
+  NewButton(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(6.0),
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.orange,
+      ),
+      child: MaterialButton(
+        onPressed: () {
+          buttonPressed(text);
+        },
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.black, fontSize: 28.0),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/NewButton.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,97 +10,143 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Calculator',
+      home: MyCalculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyCalculator extends StatefulWidget {
+  const MyCalculator({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyCalculatorState createState() => MyCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyCalculatorState extends State<MyCalculator> {
+  String output = '0';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Calculator'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+            color: Colors.black,
+            child: Column(
+              children: <Widget>[
+                Flexible(
+                  flex: 4,
+                  child: Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 10,
+                    ),
+                    child: Text(output,
+                        style: const TextStyle(
+                          fontSize: 60.0,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    color: Colors.white,
+                  ),
+                ),
+                Flexible(
+                  flex: 5,
+                  child: Container(
+                    color: Colors.black,
+                    child: Column(
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: SizedBox(
+                            height: double.infinity,
+                            width: double.infinity,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flexible(flex: 1, child: NewButton('AC')),
+                                Flexible(flex: 1, child: NewButton('')),
+                                Flexible(flex: 1, child: NewButton('%')),
+                                Flexible(flex: 1, child: NewButton('\u00F7')),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: SizedBox(
+                            height: double.infinity,
+                            width: double.infinity,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flexible(flex: 1, child: NewButton('7')),
+                                Flexible(flex: 1, child: NewButton('8')),
+                                Flexible(flex: 1, child: NewButton('9')),
+                                Flexible(flex: 1, child: NewButton('\u00D7')),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: SizedBox(
+                            height: double.infinity,
+                            width: double.infinity,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flexible(flex: 1, child: NewButton('4')),
+                                Flexible(flex: 1, child: NewButton('5')),
+                                Flexible(flex: 1, child: NewButton('6')),
+                                Flexible(flex: 1, child: NewButton('-')),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: SizedBox(
+                            height: double.infinity,
+                            width: double.infinity,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flexible(flex: 1, child: NewButton('1')),
+                                Flexible(flex: 1, child: NewButton('2')),
+                                Flexible(flex: 1, child: NewButton('3')),
+                                Flexible(flex: 1, child: NewButton('+')),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: SizedBox(
+                            height: double.infinity,
+                            width: double.infinity,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flexible(flex: 2, child: NewButton('0')),
+                                Flexible(flex: 1, child: NewButton('.')),
+                                Flexible(flex: 1, child: NewButton('=')),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )));
   }
 }
